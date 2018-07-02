a9860f793b141651a5a00a96f726a022048a429c
Macbook Air <fabelaretorcida@gmail.com>
2018-07-03 00:02:08 +0200

    Enrrutado SPA añadido a operaciones

--- src/app/views/operations/operations-routing.module.ts
+++ src/app/views/operations/operations-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {OperationsComponent} from './operations.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: OperationsComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
---
+++ src/app/views/operations/operations.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'pa-operations',
+  templateUrl: './operations.component.html',
+  styleUrls: ['./operations.component.css']
+})
+export class OperationsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
